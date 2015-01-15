7742b15c3cf69b122a966ec8b2456bf37ddeb038
mkberger <mkberger@umich.edu>
2015-01-15 00:13:08 -0500

    Create OtherCppClass.h

---
+++ Cpp/Basic/OtherCppClass.h
@@ -0,0 +1,15 @@
+#ifndef OTHERCPPCLASS_H
+#define OTHERCPPCLASS_H
+
+#include <iostream>
+#include <string>
+
+class OtherCppClass {
+  public:
+  std::string attribute;
+  void func();
+  void setElement(int);
+  private:
+  int element;
+};
+#endif
