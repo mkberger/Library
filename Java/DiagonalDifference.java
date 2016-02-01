f253937a233779d25c81972c595088f69e757df5
mkberger <mkberger@umich.edu>
2016-02-01 14:42:46 -0500

    Create DiagonalDifference.java

---
+++ Java/DiagonalDifference.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int a[][] = new int[n][n];
+        for(int a_i=0; a_i < n; a_i++){
+            for(int a_j=0; a_j < n; a_j++){
+                a[a_i][a_j] = in.nextInt();
+            }
+        }
+        // mkberger
+        int primary_diagonal, secondary_diagonal, primary_val, secondary_val, difference;
+        primary_diagonal = secondary_diagonal = primary_val = secondary_val = difference = 0;
+        for(int i = 0; i < n; i++)
+        {
+            primary_val = a[i][i];
+            secondary_val = a[i][n-1-i];
+            primary_diagonal += primary_val;
+            secondary_diagonal += secondary_val;
+            difference += (primary_val - secondary_val);
+        }
+        System.out.println(Math.abs(difference));
+    }
+}
