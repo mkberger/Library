d5f2d99059ed35cc0562215e33bf906bc74b50b6
mkberger <mkberger@umich.edu>
2016-02-18 17:45:30 -0500

    Create Lists.py

---
+++ Python/Lists.py
@@ -0,0 +1,20 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n = int(raw_input());
+L = [];
+for x in range(0, n):
+    str_list = (raw_input()).split();
+    cmd = str_list[0];
+    if cmd == 'insert':
+        L.insert(int(str_list[1]), int(str_list[2]));
+    elif cmd == 'print':
+        print(L);
+    elif cmd == 'remove':
+        L.remove(int(str_list[1]));
+    elif cmd == 'append':
+        L.append(int(str_list[1]));
+    elif cmd == 'sort':
+        L.sort();
+    elif cmd == 'pop':
+        L.pop();
+    elif cmd == 'reverse':
+        L.reverse();
