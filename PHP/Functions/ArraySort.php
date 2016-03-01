2590893997d5335bc95ebbb9afde79643e9f3b22
mkberger <mkberger@umich.edu>
2016-03-01 18:24:37 -0500

    Update ArraySort.php

--- PHP/Functions/ArraySort.php
+++ PHP/Functions/ArraySort.php
@@ -1,18 +1,18 @@
 <html>
     <p>
-	<?php
-	// Create an array with several elements in it,
-	// then sort it and print the joined elements to the screen
-    $the_array = array(4, 3, 5, 1, 2);
-    sort($the_array);
-    print join(", ", $the_array);
-	?>
-	</p>
-	<p>
-	<?php
-	// Reverse sort your array and print the joined elements to the screen
-    rsort($the_array);
-    print join (", ", $the_array);
-	?>
+		<?php
+			// Create an array with several elements in it,
+			// then sort it and print the joined elements to the screen
+		    $the_array = array(4, 3, 5, 1, 2);
+		    sort($the_array);
+		    print join(", ", $the_array);
+			?>
+			</p>
+			<p>
+			<?php
+			// Reverse sort your array and print the joined elements to the screen
+		    rsort($the_array);
+		    print join (", ", $the_array);
+		?>
 	</p>
 </html>
