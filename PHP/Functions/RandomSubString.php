69ae881a6c71ad91bb374c06d4d8b1979fa246c3
mkberger <mkberger@umich.edu>
2016-03-01 18:25:03 -0500

    Update RandomSubString.php

--- PHP/Functions/RandomSubString.php
+++ PHP/Functions/RandomSubString.php
@@ -1,19 +1,19 @@
 <html>
     <p>
-    <?php
-    // Use rand() to print a random number to the screen
-    print rand();
-    ?>
-    </p>
-    <p>
-    <?php
-    // Use your knowledge of strlen(), substr(), and rand() to
-    // print a random character from your name to the screen.
-    $name = "Matthew";
-    $len_name = strlen($name) - 1;
-    $random = rand(0, $len_name);
-    $substr_name = substr($name, $random, 1);
-    print $substr_name;
-    ?>
+        <?php
+            // Use rand() to print a random number to the screen
+            print rand();
+            ?>
+            </p>
+            <p>
+            <?php
+            // Use your knowledge of strlen(), substr(), and rand() to
+            // print a random character from your name to the screen.
+            $name = "Matthew";
+            $len_name = strlen($name) - 1;
+            $random = rand(0, $len_name);
+            $substr_name = substr($name, $random, 1);
+            print $substr_name;
+        ?>
     </p>
 </html>
