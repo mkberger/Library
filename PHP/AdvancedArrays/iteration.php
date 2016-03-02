228d414bf52a2bc9d57c64cefb5d343e753a4d4d
mkberger <mkberger@umich.edu>
2016-03-01 22:32:39 -0500

    Update iteration.php

--- PHP/AdvancedArrays/iteration.php
+++ PHP/AdvancedArrays/iteration.php
@@ -5,21 +5,20 @@
   <body>
     <p>
       <?php
-      // On the line below, create your own associative array:
+        // On the line below, create your own associative array:
         $myArray = array('good' => 'chevy',
                         'great' => 'cadillac',
                         'greatest' => 'corvette');
 
-      // On the line below, output one of the values to the page:
+        // On the line below, output one of the values to the page:
         echo $myArray['good'] . '<br><br>';
           
-      // On the line below, loop through the array and output
-      // *all* of the values to the page:
+        // On the line below, loop through the array and output
+        // *all* of the values to the page:
         echo "Grades and makes:" . '<br>';
         foreach ($myArray as $grade => $make) {
             echo $grade . ', ' . $make . '<br>';   
         }
-     
       ?>
     </p>
   </body>
