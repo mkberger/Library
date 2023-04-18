93d0966b4867decf7f192b0a0b2efd40618c7e70
mkberger <mkberger@umich.edu>
2023-04-18 13:57:58 -0400

    Update MinimumNumberOfOperationsToMoveAllBallsToEachBox.cpp

--- Cpp/LeetCode/MinimumNumberOfOperationsToMoveAllBallsToEachBox/MinimumNumberOfOperationsToMoveAllBallsToEachBox.cpp
+++ Cpp/LeetCode/MinimumNumberOfOperationsToMoveAllBallsToEachBox/MinimumNumberOfOperationsToMoveAllBallsToEachBox.cpp
@@ -1,5 +1,5 @@
-// https://leetcode.com/problems/minimum-number-of-operations-to-move-all-balls-to-each-box/description/
-// BRUTE FORCE SOLUTION
+// description of problem found at : https://leetcode.com/problems/minimum-number-of-operations-to-move-all-balls-to-each-box/description/
+// BRUTE FORCE O(N^2) SOLUTION
 class Solution {
 public:
     vector<int> minOperations(string s) {
@@ -16,4 +16,31 @@ public:
         return v;    
     }
 };
-// TIME EFFICIENT SOLUTION
+// TIME EFFICIENT O(N) SOLUTION
+class Solution {
+public:
+    vector<int> minOperations(string s) {
+        vector<int> v;
+        int moves_before = 0, moves_after = 0, n_before = 0, n_after = 0;
+        s.insert(0, "0");
+        for(int i = 0; i < s.size(); i++) {
+            if (s[i] == '1') {
+                moves_after += i;
+                n_after ++;
+            }
+        }
+        for(int i = 1; i < s.size(); i++) {
+            moves_after -= n_after;
+            if (s[i-1] == '1'){
+                n_before ++;
+                
+            }
+            moves_before += n_before;
+            if(s[i] == '1') {
+                n_after --;
+            }
+            v.push_back(moves_before + moves_after);
+        }
+        return v;   
+    }
+};
